caf2a0e203b5277eafc2dd8b0c2cd93534c82f29
Timothy Reynoso <domex2467@gmail.com>
2020-10-02 15:00:17 -0400

    completed

--- copyspecial.py
+++ copyspecial.py
@@ -7,7 +7,7 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 
 # give credits
-__author__ = "???"
+__author__ = "Timothy Reynoso with help from Gabby, Leanne, Shanquel, Sondos"
 
 import re
 import os
@@ -19,17 +19,32 @@ import argparse
 
 def get_special_paths(dirname):
     """Given a dirname, returns a list of all its special files."""
-    # your code here
-    return
+    # your code here re.search // match
+    path_list = []
+    list_files = os.listdir(dirname)
+    for file in list_files:
+        if re.search(r'__\w*__', file):
+            path_list.append(
+                os.path.abspath(os.path.join(dirname, file)))
+    return path_list
 
 
 def copy_to(path_list, dest_dir):
+    """ given a list of file paths,
+    copies those files into the given directory. """
     # your code here
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
+    for path in path_list:
+        shutil.copy(path, dest_dir)
     return
 
 
 def zip_to(path_list, dest_zip):
+    """ given a list of file paths,
+    zip those files up into the given zip path """
     # your code here
+    subprocess.run(['zip', '-j', dest_zip] + path_list)
     return
 
 
@@ -39,11 +54,18 @@ def main(args):
     parser = argparse.ArgumentParser()
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
+    parser.add_argument('fromdir', help='dest zipfile for special files')
     # TODO: add one more argument definition to parse the 'from_dir' argument
     ns = parser.parse_args(args)
 
     # TODO: you must write your own code to get the command line args.
     # Read the docs and examples for the argparse module about how to do this.
+    list_paths = get_special_paths(ns.fromdir)
+    if ns.todir:
+        copy_to(list_paths, ns.todir)
+    if ns.tozip:
+        zip_to(list_paths, ns.tozip)
+    print('\n'.join(list_paths))
 
     # Parsing command line arguments is a must-have skill.
     # This is input data validation. If something is wrong (or missing) with
